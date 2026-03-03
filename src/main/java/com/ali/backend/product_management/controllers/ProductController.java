5d15f10bdaf8559c70a73b505d363db113b004ba
Ali Chkhiro <chkhiroali97@gmail.com>
2026-03-03 11:55:57 +0000

    L'ajout des méthodes de suppression et getProductById à le contrôleur.

--- src/main/java/com/ali/backend/product_management/controllers/ProductController.java
+++ src/main/java/com/ali/backend/product_management/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,25 +30,33 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> allProducts(){
-        return productService.findAllProducts();
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts(){
+        List<ProductResponseDTO> responseDTOList = productService.findAllProducts()
+                .stream()
+                .map(productMapper::responseDTO)
+                .toList();
+        return ResponseEntity.ok(responseDTOList);
     }
 
+    // 1 - map :Product → ProductResponseDTO
+    // 2 - map :DTO → ResponseEntity.ok(dto)
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id){
-        return productService.findProductById(id).map(ResponseEntity::ok)
+    public ResponseEntity<ProductResponseDTO>getProductById(@PathVariable Long id){
+        return productService.findProductById(id)
+                .map(productMapper::responseDTO)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> create(@Valid @RequestBody ProductRequestDTO productRequestDTO){
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO productRequestDTO){
         Product product = productMapper.toEntity(productRequestDTO);
         Product savedProduct = productService.createProduct(product);
         ProductResponseDTO responseDTO = productMapper.responseDTO(savedProduct);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> update(@PathVariable Long id, @Valid @RequestBody ProductRequestDTO productRequestDTO){
+    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequestDTO productRequestDTO){
         Optional<Product> optionalProduct = productService.findProductById(id);
         if (optionalProduct.isEmpty()){
             return ResponseEntity.notFound().build();
@@ -60,8 +69,8 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ProductRequestDTO> delete(@PathVariable Long id){
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
         productService.deleteProduct(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.noContent().build();
     }
 }
